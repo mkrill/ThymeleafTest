aada005520122e7322d61306c60225cb920a4b32
mkrill <michael.krill@telekom.de>
2019-12-19 12:46:24 +0100

    first commit of local files to server

---
+++ src/test/java/de/krillonline/ThymeleafTest/ThymeleafTestApplicationTests.java
@@ -0,0 +1,13 @@
+package de.krillonline.ThymeleafTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
